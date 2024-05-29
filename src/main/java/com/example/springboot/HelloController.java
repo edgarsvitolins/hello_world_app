4095f247bf36de1632f9b81a323991b045439f67
Edgars Vitolins <vitolins.edgars@gmail.com>
2024-05-29 12:26:10 -0500

    Hello from Spring Boot! 2

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot! 2";
     }
 
 }
