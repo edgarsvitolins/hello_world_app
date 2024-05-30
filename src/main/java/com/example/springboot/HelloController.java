e3d461aabdd7a60ffb3f39625aad1d3b5f9a7364
Edgars Vitolins <vitolins.edgars@gmail.com>
2024-05-29 21:21:29 -0500

    Hello from Spring Boot BE!

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.springboot;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    @CrossOrigin
     @GetMapping("/")
     public String index() {
-        return "Hello from Spring Boot! 3";
+        return "Hello from Spring Boot BE!";
     }
 
 }
